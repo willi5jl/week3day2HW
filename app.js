f52cbe571defe0cdd27fff88cacb596be6a52c1f
Jenna <jennnawilliams513@gmail.com>
2022-01-19 00:26:40 -0500

    what

---
+++ app.js
@@ -0,0 +1,30 @@
+function sum(a,b){
+var Sum = (a+b);
+
+console.log(sum(a,b));
+return sum;
+}
+
+function getDifference(a,b){
+    var  difference = (a-b);
+
+    console.log(difference(a,b));
+    return difference;
+    
+} 
+
+function getMultiply(a,b){
+    var  multiply = (a*b);
+
+    console.log(multiply(a,b));
+    return multiply;
+    
+} 
+
+function divideBy(a,b){
+    var  divide = (a/b);
+
+    console.log(divide(a,b));
+    return divide;
+    
+} 
